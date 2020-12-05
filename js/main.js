bc12921f7f7149055e662c6efc5e7aaffe2511eb
1p6 <example@example.com>
2020-12-05 01:27:12 -0600

    Updated several things Using HTMLMediaElement.srcObject instead of .src Allow for only audio or only video feeds, or even no feed at all. Added a start button to allow switching to initializer mode before starting. Updated browserify. Removed uglify-js since it doesn't support async functions.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
-var SimplePeer = require("simple-peer");
-var $ = function(s){ return document.querySelector(s) };
+let SimplePeer = require("simple-peer");
+let $ = function(s){ return document.querySelector(s) };
 
-navigator.mediaDevices.getUserMedia({audio: true, video: true}).then(function(stream){
-	$('#vid1').src = URL.createObjectURL(stream);
+$('#start').onclick = async () => {
+	let stream1 = [], stream2 = [];
 
-	var peer = new SimplePeer({initiator: location.hash === "#1", trickle: false, stream: stream});
+	try {
+		stream1 = (await navigator.mediaDevices.getUserMedia({audio: true})).getTracks();
+	} catch(e) {
+		if(e.name !== 'NotFoundError' && e.name !== 'NotAllowedError') {
+			alert(`audio\n${e.name}\n${e.message}`);
+			throw e;
+		}
+	}
+	try {
+		stream2 = (await navigator.mediaDevices.getUserMedia({video: true})).getTracks();
+	} catch(e) {
+		if(e.name !== 'NotFoundError' && e.name !== 'NotAllowedError') {
+			alert(`video\n${e.name}\n${e.message}`);
+			throw e;
+		}
+	}
+
+	let stream = new MediaStream(stream1.concat(stream2));
+	$('#vid1').srcObject = stream;
+
+	let peer = new SimplePeer({initiator: location.hash === "#1", trickle: false, stream: stream});
 
 	$('#button').onclick = function(){
 		peer.signal(JSON.parse($('#area').value));
@@ -16,9 +36,6 @@ navigator.mediaDevices.getUserMedia({audio: true, video: true}).then(function(st
 	});
 
 	peer.on('stream', function(stream){
-		$('#vid2').src = URL.createObjectURL(stream);
+		$('#vid2').srcObject = stream;
 	});
-
-}).catch(function(err){
-	alert("Error\n" + err.message);
-});
+};
